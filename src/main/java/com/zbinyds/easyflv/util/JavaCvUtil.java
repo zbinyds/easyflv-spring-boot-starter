131dbe42f00e5688e58e5ac29fd859edf9642771
zbinyds <zb.com>
2024-05-16 09:20:18 +0800

    fix: JavaCvUtil补充

--- src/main/java/com/zbinyds/easyflv/util/JavaCvUtil.java
+++ src/main/java/com/zbinyds/easyflv/util/JavaCvUtil.java
@@ -13,10 +13,19 @@ import java.io.ByteArrayOutputStream;
  * @Create 2024-05-08 15:47
  */
 public class JavaCvUtil {
+
+    /**
+     * 创建抓图器并运行。
+     *
+     * @param url 抓取地址源
+     * @return {@link FFmpegFrameGrabber 抓图器}
+     */
     @SneakyThrows
     public static FFmpegFrameGrabber createGrabber(String url) {
         FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(url);
+        // 指定rtsp传输方式为tcp
         grabber.setOption("rtsp_transport", "tcp");
+        // 指定超时时间为5s
         grabber.setOption("stimeout", "5000000");
         // 若有必要可以限制请求方式为POST，但对应的接口请求方式也必须是POST
 //        grabber.setOption("method", "POST");
@@ -25,13 +34,23 @@ public class JavaCvUtil {
         return grabber;
     }
 
+    /**
+     * 创建取流器并运行。
+     *
+     * @param stream  待保存的流
+     * @param grabber 抓图器
+     * @return {@link FFmpegFrameRecorder 取流器}
+     */
     @SneakyThrows
     public static FFmpegFrameRecorder createRecorder(ByteArrayOutputStream stream, FFmpegFrameGrabber grabber) {
         FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(stream, grabber.getImageWidth(), grabber.getImageHeight(),
                 grabber.getAudioChannels());
         recorder.setInterleaved(true);
+        // 设置编码速度，以最快的速度编码
         recorder.setVideoOption("preset", "ultrafast");
+        // 设置最小化延迟确保实时性
         recorder.setVideoOption("tune", "zerolatency");
+        // 设置视频编码质量，值越低质量越高，0表示无损
         recorder.setVideoOption("crf", "20");
         recorder.setFrameRate(grabber.getFrameRate());
         recorder.setSampleRate(grabber.getSampleRate());
@@ -39,6 +58,7 @@ public class JavaCvUtil {
             recorder.setAudioChannels(grabber.getAudioChannels());
             recorder.setAudioBitrate(grabber.getAudioBitrate());
         }
+        // 指定输出格式为flv
         recorder.setFormat("flv");
         recorder.setVideoBitrate(grabber.getVideoBitrate());
         return recorder;
